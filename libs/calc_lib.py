468bc916904a2f90f39465242569fa24b78d9800
viprs <viprs@qq.com>
2017-06-18 15:37:19 +0800

    添加了libs文件夹

---
+++ libs/calc_lib.py
@@ -0,0 +1,14 @@
+#encoding:utf-8
+
+def Magic_calc(a, b, c=10):
+    """这是一个魔术计算"""
+    if c == 10:
+        return a+b+c
+    else:
+        return a+b-c
+
+        
+def sum(a, b):
+    return a+b
+    
+print Magic_calc(1, 2, 9)
